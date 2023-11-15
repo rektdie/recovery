b49c0b34a9b3b24076c5e7d9728e8bdd46698d6e
rektdie <rektdie@gmail.com>
2023-11-15 22:05:35 +0100

    Added some print outputs

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
 import shutil
+import time
 
-path = "F:/Munka/14i/A_cs/Kasa Mark/PowerPoint/Geoguessr.pptx"
+path = "M:/Munka/14i/A_cs/Kasa Mark/PowerPoint/Geoguessr.pptx"
 destination = "D:/Geoguessr.pptx"
 
 def MoveFiles():
+    start = time.perf_counter()
     shutil.copyfile(path, destination)
+    end = time.perf_counter()
+    print(f"Moved in {end-start}")
 
-while True:
-    if os.path.exists(path):
-        MoveFiles()
-        break
+def Main():
+    print("Waiting")
+    while True:
+        if os.path.exists(path):
+            MoveFiles()
+            break
+
+if __name__ == "__main__":
+    Main()
